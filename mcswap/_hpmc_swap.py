491a7669771a508ec0f935677c7bdd222eda7239
mrh5506 <muhammadrisyadh@gmail.com>
2020-03-11 11:51:00 -0700

    Deleted mcswap folder

--- mcswap/_hpmc_swap.py
+++
@@ -1,5 +0,0 @@
-# This file exists to allow the hoomd module to import from the source checkout dir
-# for use when building the sphinx documentation.
-
-class sph_poly_param_proxy(object):
-    pass
